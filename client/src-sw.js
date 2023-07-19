e7289d18e2b9a226a7a60f5d35ff18c85cbe78fe
robstowe <robstowe94@gmail.com>
2023-07-19 13:02:44 -0700

    found the error, needed to instal a babel plugin

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,10 +1,12 @@
-const { warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+
 precacheAndRoute(self.__WB_MANIFEST);
+
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -16,20 +18,13 @@ const pageCache = new CacheFirst({
     }),
   ],
 });
+
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-registerRoute(
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate({
-    cacheName: 'asset-cache',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [0, 200],
-      }),
-    ],
-  })
-);
 
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// TODO: Implement asset caching
+registerRoute();
